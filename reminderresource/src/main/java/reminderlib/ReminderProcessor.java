3898f85638cef4645ef7673e1cbed147c8b4f569
MojunZhu <mz2330@columbia.edu>
2015-07-27 22:39:56 -0700

    some modification

--- reminderresource/src/main/java/reminderlib/ReminderProcessor.java
+++ reminderresource/src/main/java/reminderlib/ReminderProcessor.java
@@ -1,5 +1,7 @@
 package reminderlib;
 
+import java.util.List;
+
 import com.mojun.reminder.reminderdataobj.CreateReminderEventRequest;
 import com.mojun.reminder.reminderdataobj.CreateReminderUserRequest;
 import com.mojun.reminder.reminderdataobj.ReminderEvent;
@@ -7,38 +9,70 @@ import com.mojun.reminder.reminderdataobj.ReminderEventList;
 import com.mojun.reminder.reminderdataobj.ReminderUser;
 
 public class ReminderProcessor {
+	public static final String SERVICE_CONFIG_PATH = "reminderresource/service/config"; 
 	
 	public static ReminderProcessor getDefaultProcessor() {
 		return new ReminderProcessor();
 	}
 	
 	public ReminderEvent createReminderEvent(String userId, CreateReminderEventRequest request) {
-		//TO DO
-		return new ReminderEvent();
+		assert userId != null;
+		assert request != null;
+		
+		DBReminderEventObj dbReminderEventObj = upsertDBReminderEvent(request);
+		ReminderEvent result = constructEvent(dbReminderEventObj);
+		
+		return result;
 	}
 	
 	public ReminderUser createReminderUser(CreateReminderUserRequest request) {
-		//TO DO
-		return new ReminderUser();
+		assert request != null;
+		
+		DBReminderUser dbReminderUser = upsertDBReminderUser(request);
+		ReminderUser result = constructUser(dbReminderUser);
+		
+		return result;
 	}
 	
 	public ReminderEvent getSingleReminderEvent(String userId, String reminderEventId) {
-		//TO DO
-		return new ReminderEvent();
+		assert userId != null;
+		assert reminderEventId != null;
+		
+		DBReminderEventDAO reminderEventDAO = DBReminderEventDAO.getinstance();
+		DBReminderEvent dbReminderEvent = reminderEventDAO.getEventbyId(userId, reminderEventId);
+		
+		ReminderEvent result = constructEvent(dbReminderEventObj);
+		return result;
 	}
 	
 	public ReminderEventList getReminderEventList(String userId) {
-		//TO DO
-		return new ReminderEventList();
+		assert userId != null;
+		
+		DBReminderEventDAO dbReminderEventDAO = DBReminderEventDAO.getinstance();
+		List<DBReminderEvent> dbReminderEventlist = dbReminderEventDAO.getEventsbyId(userId);
+		
+		ReminderEventList result = constructEventList(dbReminderEventlist);
+		
+		return result;
 	}
 	
 	public ReminderEvent updateSingleEvent(String userId, String reminderEventId, CreateReminderEventRequest request) {
-		//TO DO
-		return new ReminderEvent();
+		assert userId != null;
+		assert reminderEventId != null;
+		assert request != null;
+		
+		DBReminderEventDAO dbReminderEventDAO = DBReminderEventDAO.getinstance();
+		DBReminderEvent dbReminderEvent = dbReminderEventDAO.getEventById(userId, reminderEventId);
+		
+		DBReminderEventObj dbReminderEventObj = upsertDBReminderEvent(request);
+		ReminderEvent result = constructEvent(dbReminderEventObj);
+		return result;
 	}
 	
 	public ReminderUser updateReminderUser(String userId, CreateReminderUserRequest request) {
-		//TO DO
+		assert userId != null;
+		assert request != null;
+		
 		return new ReminderUser();
 	}
 
